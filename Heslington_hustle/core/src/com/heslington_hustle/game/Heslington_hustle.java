5f1a81f7563dd216538c6500af140a475b5ff959
JamesLeney <jl3667@york.ac.uk>
2024-03-05 10:10:03 +0000

    Map and sprites imported

--- Heslington_hustle/core/src/com/heslington_hustle/game/Heslington_hustle.java
+++ Heslington_hustle/core/src/com/heslington_hustle/game/Heslington_hustle.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -16,35 +19,43 @@ public class Heslington_hustle extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private OrthographicCamera camera;
 	private Rectangle playerModel;
+	private TiledMap map;
+	private OrthogonalTiledMapRenderer renderer;
+
 
 	@Override
 	public void create() {
-		// load the images for the droplet and the bucket, 64x64 pixels each
-		player = new Texture(Gdx.files.internal("player.png"));
+		// load the image for player, 64x64 pixels
+		player = new Texture(Gdx.files.internal("char1d.png"));
+
+		// load the map
+		map = new TmxMapLoader().load("map1.tmx");
+		renderer = new OrthogonalTiledMapRenderer(map);
 
 		// create the camera and the SpriteBatch
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
+		camera.setToOrtho(false, 800, 800);
 		batch = new SpriteBatch();
 
 		// create a Rectangle to logically represent the bucket
 		playerModel = new Rectangle();
 		playerModel.x = 800 / 2 - 64 / 2; // center the player horizontally
-		playerModel.y = 480 / 2 - 64 / 2; // center the player vertically
+		playerModel.y = 800 / 2 - 64 / 2; // center the player vertically
 		playerModel.width = 64;
 		playerModel.height = 64;
 
 	}
 	@Override
 	public void render() {
-		// clear the screen with a dark blue color. The
-		// arguments to clear are the red, green
-		// blue and alpha component in the range [0,1]
-		// of the color to be used to clear the screen.
-		ScreenUtils.clear(0, 0, 0.2f, 1);
+
+		// clear the screen
+		ScreenUtils.clear(0.7f, 0.7f, 1.0f, 1);
 
 		// tell the camera to update its matrices.
 		camera.update();
+
+		renderer.setView(camera);
+		renderer.render();
 
 		// tell the SpriteBatch to render in the
 		// coordinate system specified by the camera.
@@ -72,7 +83,7 @@ public class Heslington_hustle extends ApplicationAdapter {
 		if(playerModel.x < 0) playerModel.x = 0;
 		if(playerModel.x > 800 - 64) playerModel.x = 800 - 64;
 		if(playerModel.y < 0) playerModel.y = 0;
-		if(playerModel.y > 480 - 64) playerModel.y = 480 - 64;
+		if(playerModel.y > 800 - 64) playerModel.y = 480 - 64;
 	}
 
 	@Override
