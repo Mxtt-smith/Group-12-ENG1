0c68127727beb14cae6d49a85a9526837c94c35d
Daniel Agar <da1098@york.ac.uk>
2024-03-05 10:34:05 +0000

    Correcting dependencies

---
+++ Heslington_hustle/desktop/build/resources/main/maps/Water.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Water" tilewidth="16" tileheight="16" tilecount="4" columns="4">
+ <image source="Water.png" width="64" height="16"/>
+ <wangsets>
+  <wangset name="Random water" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="1" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="2" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="3" wangid="1,1,1,1,1,1,1,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
