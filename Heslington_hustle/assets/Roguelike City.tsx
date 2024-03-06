161d1387ae0af89e9f55b28474e43474bcc70b23
Daniel Agar <da1098@york.ac.uk>
2024-03-06 15:59:55 +0000

    Changed and improved assets

---
+++ Heslington_hustle/assets/Roguelike City.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Roguelike City" tilewidth="16" tileheight="16" spacing="1" tilecount="1036" columns="37">
+ <image source="Assets/Roguelike Modern City Pack/Spritesheet/roguelikeCity_magenta.png" width="628" height="475"/>
+</tileset>
---
+++ Heslington_hustle/assets/Tilled_Dirt.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Tilled_Dirt" tilewidth="16" tileheight="16" tilecount="77" columns="11">
+ <image source="Assets/Sprout Lands - Sprites - Basic pack/Tilesets/Tilled_Dirt.png" width="176" height="112"/>
+ <wangsets>
+  <wangset name="Path" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="12" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="55" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="56" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="57" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="66" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="67" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="68" wangid="1,1,1,1,1,1,1,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
