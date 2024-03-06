64f2ad712718c11344a297f73b6fcf013d519998
D-Agar <da1098@york.ac.uk>
2024-03-06 13:49:23 +0000

    Added ApplicationListener

--- Heslington_hustle/core/src/com/heslington_hustle/game/Heslington_hustle.java
+++ Heslington_hustle/core/src/com/heslington_hustle/game/Heslington_hustle.java
@@ -1,2 +1,35 @@
 package com.heslington_hustle.game;
 
+import com.badlogic.gdx.ApplicationListener;
+
+public class Heslington_hustle implements ApplicationListener {
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
