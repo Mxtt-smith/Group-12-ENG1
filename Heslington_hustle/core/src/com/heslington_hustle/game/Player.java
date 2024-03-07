0e060490e6f30cb85fa129f38b8f55354c474a15
Daniel Agar <da1098@york.ac.uk>
2024-03-07 15:29:50 +0000

    Optimised Player class

--- Heslington_hustle/core/src/com/heslington_hustle/game/Player.java
+++ Heslington_hustle/core/src/com/heslington_hustle/game/Player.java
@@ -1,25 +1,14 @@
 package com.heslington_hustle.game;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 import java.awt.*;
 
 public class Player extends Sprite{
-    int x;
-    int y;
-    int xSpeed;
-    int ySpeed;
-    Texture texture;
     Rectangle playerShape;
     TextureAtlas textureAtlas;
-    SpriteBatch spriteBatch;
     Sprite playerSprite;
-    int wWidth = Gdx.graphics.getWidth();
-    int wHeight = Gdx.graphics.getHeight();
 
     public Player(TextureAtlas atlas) {
         super(atlas.getRegions().get(0));
