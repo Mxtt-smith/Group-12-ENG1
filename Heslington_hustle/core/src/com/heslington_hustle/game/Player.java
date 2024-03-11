3ed8da812bc0f5fd2093580110889c9903266a4b
Daniel Agar <da1098@york.ac.uk>
2024-03-11 18:46:46 +0000

    Altered map and created a basic object collision

--- Heslington_hustle/core/src/com/heslington_hustle/game/Player.java
+++ Heslington_hustle/core/src/com/heslington_hustle/game/Player.java
@@ -1,51 +1,81 @@
 package com.heslington_hustle.game;
 
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 
 import java.awt.*;
 
-public class Player extends Sprite{
+public class Player extends Sprite {
     Rectangle playerShape;
     TextureAtlas textureAtlas;
     Sprite playerSprite;
     public static String character;
+    private final TiledMapTileLayer collisionLayer;
     String direction;
 
-    public Player(TextureAtlas atlas) {
+    public Player(TextureAtlas atlas, TiledMapTileLayer gameCollisionLayer) {
         super(atlas.getRegions().get(0));
         direction = "DOWN";
+        this.collisionLayer = gameCollisionLayer;
         textureAtlas = atlas;
         playerShape = new Rectangle(16, 16);
         playerSprite = new Sprite(textureAtlas.findRegion(character + "sd"));
     }
 
     public void setTexture(String textureName) {
-        setRegion(textureAtlas.findRegion(textureName));
+        playerSprite.setRegion(textureAtlas.findRegion(textureName));
+    }
+
+    public boolean collision(int transX, int transY) {
+        float oldX = playerSprite.getX();
+        float oldY = playerSprite.getY();
+        System.out.println("Current x:" + oldX);
+        System.out.println("Current y:" + oldY);
+
+
+        // get the collision tile for the corresponding future coordinate
+        // Catch the null exception - no tile exists there
+        try {
+            // convert the coordinates into cell number, bottom left is 0, 0
+            // map is 50 x 50 cells
+            int cellX = (int) (oldX + transX) / 16;
+            int cellY = (int) (oldY + transY) / 16;
+            System.out.println("Cell: " + cellX + "," + cellY);
+            TiledMapTileLayer.Cell cell = collisionLayer.getCell(cellX, cellY);
+            return cell.getTile().getProperties().containsKey("Collidable");
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     // Movement methods
     public void moveUp() {
-        this.translateY(2);
+        playerSprite.translateY(2);
         this.setTexture(character+"u");
         this.direction = "UP";
     }
     public void moveDown() {
-        this.translateY(-2);
+        playerSprite.translateY(-2);
         this.setTexture(character+"d");
         this.direction = "DOWN";
     }
 
     public void moveLeft() {
-        this.translateX(-2);
+        playerSprite.translateX(-2);
         this.setTexture(character+"l");
         this.direction = "LEFT";
     }
 
     public void moveRight() {
-        this.translateX(2);
+        playerSprite.translateX(2);
         this.setTexture(character+"r");
         this.direction = "RIGHT";
+    }
+
+    public void setPosition(float x, float y) {
+        playerSprite.setPosition(x, y);
     }
 
     public void stationary() {
@@ -63,5 +93,10 @@ public class Player extends Sprite{
                 this.setTexture(character+"sr");
                 break;
         }
+    }
+
+    @Override
+    public void draw(Batch batch) {
+        playerSprite.draw(batch);
     }
 }
--- Heslington_hustle/desktop/build/resources/main/Grass.tsx
+++ Heslington_hustle/desktop/build/resources/main/Grass.tsx
@@ -1,6 +1,98 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Grass" tilewidth="16" tileheight="16" tilecount="77" columns="11">
  <image source="Grass.png" width="176" height="112"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="0" width="16.005" height="4.8177">
+    <polygon points="0,0 -16.005,0 -16.005,4.8177 0,4.8177"/>
+   </object>
+   <object id="2" x="4.87496" y="2.01918" width="4.87998" height="13.9808">
+    <polygon points="0,0 -4.87998,0 -4.87998,13.9808 0,13.9808"/>
+   </object>
+   <object id="3" x="7.24179" y="0" width="7.22499" height="8.22154">
+    <polygon points="0,0 -7.22499,0 -7.22499,8.22154 0,8.22154"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="2.36591"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="0" y="0" width="16" height="4.77193">
+    <polygon points="0,0 16,0 16,4.77193 0,4.77193"/>
+   </object>
+   <object id="4" x="11.1664" y="2" width="4.83362" height="14">
+    <polygon points="0,0 4.83362,0 4.83362,14 0,14"/>
+   </object>
+   <object id="5" x="8.82206" y="0" width="7.15635" height="8.14344">
+    <polygon points="0,0 7.15635,0 7.15635,8.14344 0,8.14344"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="0" y="0" width="3.71236" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13.3256" y="0" width="2.67438" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="9.62406" y="9.50067" width="6.37594" height="6.49933"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="10.4261" width="5.57393" height="5.57393"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="16" width="16" height="4.77193">
+    <polygon points="0,0 -16,0 -16,-4.77193 0,-4.77193"/>
+   </object>
+   <object id="2" x="4.83362" y="14" width="4.83362" height="14">
+    <polygon points="0,0 -4.83362,0 -4.83362,-14 0,-14"/>
+   </object>
+   <object id="3" x="7.17796" y="16" width="7.15635" height="8.14344">
+    <polygon points="0,0 -7.15635,0 -7.15635,-8.14344 0,-8.14344"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="12.7087" width="16" height="3.29131"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="15.9894" width="15.9795" height="4.76583">
+    <polygon points="0,0 15.9795,0 15.9795,-4.76583 0,-4.76583"/>
+   </object>
+   <object id="2" x="11.1521" y="13.9919" width="4.82744" height="13.9821">
+    <polygon points="0,0 4.82744,0 4.82744,-13.9821 0,-13.9821"/>
+   </object>
+   <object id="3" x="8.81078" y="15.9894" width="7.1472" height="8.13303">
+    <polygon points="0,0 7.1472,0 7.1472,-8.13303 0,-8.13303"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.6728" y="0" width="5.32716" height="6.00578"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="5.0187" height="5.94409"/>
+  </objectgroup>
+ </tile>
  <wangsets>
   <wangset name="Ground" type="mixed" tile="-1">
    <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
