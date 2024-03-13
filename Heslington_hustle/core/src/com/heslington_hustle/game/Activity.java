e3d138e4e490a732754797f44e00fcee79fe76e3
Daniel Agar <da1098@york.ac.uk>
2024-03-13 21:01:21 +0000

    Fixed merge

--- Heslington_hustle/core/src/com/heslington_hustle/game/Activity.java
+++ Heslington_hustle/core/src/com/heslington_hustle/game/Activity.java
@@ -20,11 +20,8 @@ public class Activity {
     public void set(float x, float y, float width, float height) {
         zone.set(x, y, width, height);
     }
-<<<<<<< HEAD
-=======
 
     public ActivityType getType() {
         return type;
     }
->>>>>>> origin/J1.2
 }
